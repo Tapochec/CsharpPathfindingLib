5764a9182bce3a4296dad0c208536da1a3c0fe00
Andrew <and_petr_00@mail.ru>
2022-06-12 22:34:39 +0300

    Add node type arg in IGraph.AddNodes method
    Also add xmldoc to the IGraph.AddNodes method

--- CSPF/Common/IGraph.cs
+++ CSPF/Common/IGraph.cs
@@ -5,7 +5,11 @@
         uint ActualWidth { get; }
         uint ActualHeight { get; }
 
-        void AddNodes(params TPosition[] positions);
+        /// <summary>
+        /// Creates new nodes with corresponding positions.
+        /// </summary>
+        /// <param name="nodeType">The type of all new nodes</param>
+        void AddNodes(INodeType nodeType, params TPosition[] positions);
         void RemoveNodes(params TPosition[] positions);
         void Clear(bool resetCanvasSize = true);
         IReadOnlyList<INode> GetNeighbors(INode node, bool includeNullCostNodes = true);
