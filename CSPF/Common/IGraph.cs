e041cdb7202f3e8d5a047e47c2d0f2c12a5792ca
Andrew <and_petr_00@mail.ru>
2022-06-12 17:01:19 +0300

    Add IGraph interface

---
+++ CSPF/Common/IGraph.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPF.Common
+{
+    public interface IGraph<TPosition>
+    {
+        int ActualWidth { get; }
+        int ActualHeight { get; }
+        int CanvasWidth { get; }
+        int CanvasHeight { get; }
+
+        void AddNodes(params TPosition[] positions);
+        void RemoveNodes(params TPosition[] positions);
+        void Clear(bool resetCanvasSize = true);
+        IReadOnlyList<INode> GetNeighbors(INode node, bool includeNullCostNodes = true);
+    }
+}
