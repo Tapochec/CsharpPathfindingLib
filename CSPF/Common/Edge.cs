3b0becf5558ce1d943568a3c848a389c43a8af37
Andrew <and_petr_00@mail.ru>
2022-06-12 18:58:05 +0300

    Add edge realization
    Add IEdge inteface, Edge class, EdgeType enum

---
+++ CSPF/Common/Edge.cs
@@ -0,0 +1,15 @@
+﻿namespace CSPF.Common
+{
+    public class Edge : IEdge
+    {
+        public INode NodeA { get; set; }
+        public INode NodeB { get; set; }
+        public EdgeType Type { get; set; } = EdgeType.TwoWay;
+
+        public Edge(INode nodeA, INode nodeB)
+        {
+            NodeA = nodeA;
+            NodeB = nodeB;
+        }
+    }
+}
---
+++ CSPF/Common/IEdge.cs
@@ -0,0 +1,9 @@
+﻿namespace CSPF.Common
+{
+    public interface IEdge
+    {
+        INode NodeA { get; set; }
+        INode NodeB { get; set; }
+        EdgeType Type { get; set; }
+    }
+}
